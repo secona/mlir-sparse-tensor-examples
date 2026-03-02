d8a185ba8b8dfb922a7575500b77d0d9cd423f09
Vito Secona <secona00@gmail.com>
2026-03-02 11:00:31 +0700

    transpose matrix

--- main.cpp
+++ main.cpp
@@ -1,75 +1,73 @@
 #include <cstdint>
+#include <cstdlib>
+#include <iostream>
 #include <mlir/Dialect/SparseTensor/IR/Enums.h>
 #include <mlir/ExecutionEngine/CRunnerUtils.h>
-#include <mlir/ExecutionEngine/SparseTensorRuntime.h>
 #include <mlir/ExecutionEngine/SparseTensor/File.h>
-#include <cstdlib>
+#include <mlir/ExecutionEngine/SparseTensorRuntime.h>
+#include <string>
 
 struct MemRefDescriptor2D {
-  float* allocated;
-  float* aligned;
+  float *allocated;
+  float *aligned;
   int64_t offset;
   int64_t sizes[2];
   int64_t strides[2];
 };
 
 extern "C" {
-    void _mlir_ciface_tensor_add(MemRefDescriptor2D *out, void *a, void *b);
+void _mlir_ciface_tensor_add(MemRefDescriptor2D *out, void *a);
 }
 
-void* create_empty_csr_tensor() {
-    uint64_t sizes_data[] = {32, 32};
-    StridedMemRefType<uint64_t, 1> sizes_memref = {sizes_data, sizes_data, 0, {2}, {1}};
+void *create_csr_tensor(std::string filename) {
+  uint64_t sizes_data[] = {32, 32};
+  StridedMemRefType<uint64_t, 1> sizes_memref = {
+      sizes_data, sizes_data, 0, {2}, {1}};
 
-    LevelType lvl_types_data[] = {LevelType(LevelFormat::Dense), LevelType(LevelFormat::Compressed)};
-    StridedMemRefType<LevelType, 1> lvl_types_memref = {lvl_types_data, lvl_types_data, 0, {2}, {1}};
+  LevelType lvl_types_data[] = {LevelType(LevelFormat::Dense),
+                                LevelType(LevelFormat::Compressed)};
+  StridedMemRefType<LevelType, 1> lvl_types_memref = {
+      lvl_types_data, lvl_types_data, 0, {2}, {1}};
 
-    uint64_t mapping_data[] = {0, 1};
-    StridedMemRefType<uint64_t, 1> mapping_memref = {mapping_data, mapping_data, 0, {2}, {1}};
+  uint64_t mapping_data[] = {0, 1};
+  StridedMemRefType<uint64_t, 1> mapping_memref = {
+      mapping_data, mapping_data, 0, {2}, {1}};
 
-    uint64_t dimRank = 2;
-    uint64_t dimShape[2] = {32, 32};
-    PrimaryType valTp = PrimaryType::kF32;
-    SparseTensorReader *reader = SparseTensorReader::create("ibm32.mtx", dimRank, dimShape, valTp);
+  uint64_t dimRank = 2;
+  uint64_t dimShape[2] = {32, 32};
+  PrimaryType valTp = PrimaryType::kF32;
+  SparseTensorReader *reader =
+      SparseTensorReader::create(filename.c_str(), dimRank, dimShape, valTp);
 
-    void *tensor = _mlir_ciface_newSparseTensor(
-        &sizes_memref, 
-        &sizes_memref, 
-        &lvl_types_memref, 
-        &mapping_memref, 
-        &mapping_memref, 
-        mlir::sparse_tensor::OverheadType::kIndex, 
-        mlir::sparse_tensor::OverheadType::kIndex, 
-        mlir::sparse_tensor::PrimaryType::kF32, 
-        mlir::sparse_tensor::Action::kFromReader, 
-        reader
-    );
+  void *tensor = _mlir_ciface_newSparseTensor(
+      &sizes_memref, &sizes_memref, &lvl_types_memref, &mapping_memref,
+      &mapping_memref, mlir::sparse_tensor::OverheadType::kIndex,
+      mlir::sparse_tensor::OverheadType::kIndex, valTp,
+      mlir::sparse_tensor::Action::kFromReader, reader);
 
-    delSparseTensorReader(reader);
+  delSparseTensorReader(reader);
 
-    return tensor;
+  return tensor;
 }
 
 int main() {
-    void *sparse_tensor_a = create_empty_csr_tensor();
-    void *sparse_tensor_b = create_empty_csr_tensor();
-    MemRefDescriptor2D out;
+  void *sparse_tensor_a = create_csr_tensor("ibm32.mtx");
+  MemRefDescriptor2D out;
 
-    _mlir_ciface_tensor_add(&out, sparse_tensor_a, sparse_tensor_b);
+  _mlir_ciface_tensor_add(&out, sparse_tensor_a);
 
-    delSparseTensor(sparse_tensor_a);
-    delSparseTensor(sparse_tensor_b);
+  delSparseTensor(sparse_tensor_a);
 
-    for (int i = 0; i < 32; i++) {
-        for (int j = 0; j < 32; j++) {
-            int idx = i * 32 + j;
-            if (out.aligned[idx] == 0)
-                std::cout << "  ";
-            else 
-                std::cout << out.aligned[idx] << " ";
-        }
-        std::cout << "\n";
+  for (int i = 0; i < 32; i++) {
+    for (int j = 0; j < 32; j++) {
+      int idx = i * 32 + j;
+      if (out.aligned[idx] == 0)
+        std::cout << "  ";
+      else
+        std::cout << out.aligned[idx] << " ";
     }
+    std::cout << "\n";
+  }
 
-    std::free(out.allocated);
+  std::free(out.allocated);
 }
--- tensor_add.mlir
+++ tensor_add.mlir
@@ -1,14 +1,17 @@
 #CSR = #sparse_tensor.encoding<{ map = (d0, d1) -> (d0: dense, d1: compressed) }>
 
-func.func @tensor_add(%arg0: tensor<32x32xf32, #CSR>,
-                      %arg1: tensor<32x32xf32, #CSR>) -> tensor<32x32xf32> attributes {llvm.emit_c_interface} {
-  %cst = arith.constant 0.0 : f32
-  %init = tensor.empty() : tensor<32x32xf32>
-  %out = linalg.fill ins(%cst : f32) outs(%init : tensor<32x32xf32>) -> tensor<32x32xf32>
+func.func @tensor_add(%arg0: tensor<32x32xf32, #CSR>) -> tensor<32x32xf32> attributes {llvm.emit_c_interface} {
+  %out = tensor.empty() : tensor<32x32xf32>
 
-  %3 = linalg.add
-      ins(%arg0, %arg1 : tensor<32x32xf32, #CSR>, tensor<32x32xf32, #CSR>)
-      outs(%out : tensor<32x32xf32>) -> tensor<32x32xf32>
+  %T_out = tensor.empty() : tensor<32x32xf32, #CSR>
+  %T = linalg.transpose
+    ins(%arg0 : tensor<32x32xf32, #CSR>)
+    outs(%T_out : tensor<32x32xf32, #CSR>)
+    permutation = [1, 0]
 
-  return %3 : tensor<32x32xf32>
+  %result = linalg.add
+    ins(%arg0, %arg0 : tensor<32x32xf32, #CSR>, tensor<32x32xf32, #CSR>)
+    outs(%out : tensor<32x32xf32>) -> tensor<32x32xf32>
+
+  return %result : tensor<32x32xf32>
 }
